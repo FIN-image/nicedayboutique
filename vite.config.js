e897b064b70081c3816aae36222b3c965f2382ff
blessingsmart <bublessingsmart@gmail.com>
2024-01-19 21:56:29 +0100

    modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/component/Testimonial.jsx 	modified:   vite.config.js     added the splidesjs in Testimonial

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@splidejs/splide': '/splide-4.1.3/dist/js/splide.min.js',
+    },
+  },
 })
